4a46099e0ab608f0757c825b4e5448f3a0ff8400
Lakhdar Benzahia <lakhdar.benzahia@gmail.com>
2017-08-03 16:09:24 +0100

    setting up ibnmalk project (#1)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ibnmalk
+# Ibnmalk
 Ibnmalk is a Pythonic library offers Arabic NLP functionalities such as tokenizer, POS taggers, stopwords, stemmer/lemmatizer, etc. .
---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='ibnmalk',
+      version='0.1',
+      description="Ibnmalk is a pythonic library for Arabic NLP",
+      url='https://github.com/araby360/ibnmalk',
+      author='Lakhdar Benzahia',
+      author_email='lakhdar.benzahia@gmail.com',
+      license='MIT',
+      packages=['ibnmalk'],
+      zip_safe=False)
